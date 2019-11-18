42190352ddc2046e120f73a1a72fe37564e73dc8
AYuan <lzy2118053626@hotmaill.com>
2019-11-18 22:22:01 +0800

    update

--- app/src/main/java/com/lenovo/manufacture/TestActivity.java
+++ app/src/main/java/com/lenovo/manufacture/TestActivity.java
@@ -9,6 +9,11 @@ import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 
+import com.lenovo.manufacture.bean.SalesReportBean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * @author Amoly
  * @date 2019/10/24.
@@ -20,6 +25,7 @@ public class TestActivity extends BaseActivity {
     private static final String TAG = "TestActivity";
     private FrameLayout fragmentLayout;
     private TWebView webView;
+    private ArrayList<SalesReportBean> salesReportBeans;
 
 
     @Override
@@ -40,8 +46,9 @@ public class TestActivity extends BaseActivity {
     @Override
     public void addJavaScriptInterface() {
         String localClassName = getLocalClassName();
-        Log.i(TAG, "哈哈哈：" + localClassName);
-        webView.addJavascriptInterface(new TestJavaScript(TestActivity.this), localClassName);
+        TestJavaScript obj = new TestJavaScript(TestActivity.this);
+        obj.getData();
+        webView.addJavascriptInterface(obj, localClassName);
     }
 
     private class TestJavaScript {
@@ -54,6 +61,28 @@ public class TestActivity extends BaseActivity {
         @JavascriptInterface
         public void onBack() {
             finish();
+        }
+
+        /**
+         * 模拟生成数据
+         *
+         * @return
+         */
+        @JavascriptInterface
+        public String[] getData() {
+            salesReportBeans = new ArrayList<>();
+            //生成对象集合
+            for (int i = 1; i <= 100; i++) {
+                salesReportBeans.add(new SalesReportBean(i + "哈哈汽车", Math.pow(i * 100, 2), new Date().toLocaleString(), i + ""));
+            }
+
+            String[] saleReports = new String[salesReportBeans.size()];
+
+            for (int i = 0; i < salesReportBeans.size(); i++) {
+                saleReports[i] = salesReportBeans.get(i).toString();
+            }
+            Log.i(TAG, "哈哈哈：" + saleReports.toString());
+            return saleReports;
         }
     }
 }
--- app/src/main/java/com/lenovo/manufacture/bean/SalesReportBean.java
+++ app/src/main/java/com/lenovo/manufacture/bean/SalesReportBean.java
@@ -2,5 +2,51 @@ package com.lenovo.manufacture.bean;
 
 public class SalesReportBean {
     private String carName;
-    private String price;
+    private double price;
+    private String outTime;
+    private String outCount;
+
+    public SalesReportBean(String carName, double price, String outTime, String outCount) {
+        this.carName = carName;
+        this.price = price;
+        this.outTime = outTime;
+        this.outCount = outCount;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getOutTime() {
+        return outTime;
+    }
+
+    public void setOutTime(String outTime) {
+        this.outTime = outTime;
+    }
+
+    public String getOutCount() {
+        return outCount;
+    }
+
+    public void setOutCount(String outCount) {
+        this.outCount = outCount;
+    }
+
+    @Override
+    public String toString() {
+        return carName + "," + price + "," + outTime + "," + outCount;
+    }
 }
